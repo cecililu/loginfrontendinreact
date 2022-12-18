f23a7dc909fb146e3f4fa8753f973e5bf784c9c6
cecil <cecilghimire@gmail.com>
2022-12-18 22:34:18 +0545

    tabs routing mui for login and registations

--- src/Components/pages/auth/LoginRegister.js
+++ src/Components/pages/auth/LoginRegister.js
@@ -1,9 +1,38 @@
 import { Card, Grid, Tabs, Typography,Box ,Tab } from '@mui/material'
 import image from '../../../images/car.png'
-import React from 'react'
+import React, { useState } from 'react'
+import { Login } from './Login'
+import { Registration } from './Registration'
+
+const TabPanel=(props)=>{
+
+   const {children,value,index}=props 
+   return (
+   <div role='tabpanel' hidden={value!==index}>
+        {value=== index &&(
+            <Box>
+                {children}
+            </Box>
+        )}
+   </div>
+   )
+
+}
+
+
 
 export const LoginRegister = () => {
-  return (
+  
+  const [value, setvalue] = useState(0)
+  
+  const handlechange=()=>{
+       if (value==0){
+        setvalue(1)
+       }else{
+        setvalue(0)
+       }   
+  }
+    return (
     <>
       <Grid container sx={{height:'90vh'}}>
     
@@ -24,11 +53,14 @@ export const LoginRegister = () => {
             <Card sx={{width:'100%',height:'100%',backgroundColor:'white'}}>
                 <Box>
                     <Box sx={{borderTop:1,borderBottom:1,borderColor:'divider'}}> 
-                        <Tabs> 
+                        <Tabs value={value} onChange={handlechange}> 
                             <Tab label="Login" sx={{textTransform:'none',fontWeight:'bold'}}></Tab>
                             <Tab label="Registrations"  sx={{textTransform:'none',fontWeight:'bold'}}></Tab>      
                         </Tabs>
+                        <TabPanel value={value} index={0}><Login/></TabPanel>
+                        <TabPanel value={value} index={1}><Registration/></TabPanel>
                     </Box>
+                    
                 </Box>
                 </Card>
         </Grid>
