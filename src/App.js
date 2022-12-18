7ac4a4fca353aeaae766b1615c9ad84baa726585
cecil <cecilghimire@gmail.com>
2022-12-18 17:31:05 +0545

    navbar home layout compnents added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Layout } from "./Components/pages/Layout";
+
 
 function App() {
   return (
     <div className="App">
-              
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Layout/>}/>
+          </Routes>
+        </BrowserRouter>  
     </div>
   );
 }
---
+++ src/Components/pages/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavBar } from '../NavBar'
+import { Home } from './Home'
+
+export const Layout = () => {
+  return (
+  <>
+  <NavBar/>
+  <Home/>
+  
+  </>  )
+}
